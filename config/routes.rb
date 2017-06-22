fe768b8dbe4d9f8999bb1591b5f223f9c62f5361
pesu1234 <k.takafumi1110@gmail.com>
2017-06-22 18:08:31 +0900

    Add users_controller routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'recipes#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :new, :create]
   resources :recipes
 end
