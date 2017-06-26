711458e4e5b8f2ae5f32f5b8f5661a9f4536832e
pesu1234 <k.takafumi1110@gmail.com>
2017-06-26 15:39:26 +0900

    Fix user registration new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,29 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :mail, :postnum, :birthday, :sex, :password)
   end
 end
