c9722fec639d55f727d8d92d316345c1fc348692
pesu1234 <k.takafumi1110@gmail.com>
2017-06-27 13:38:03 +0900

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy' #動作確認用
   delete '/logout' => 'sessions#destroy'
   resources :users, only: [:edit, :update, :new, :create]
+  get '/users/:id/mail/edit' => 'users#mail_edit'
+  post '/users/:id/mail/edit' => 'users#mail_update'
+  get '/users/:id/change_password' => 'users#password_edit'
+  post '/users/:id/change_password' => 'users#password_update'
   resources :recipes
   get '/recipes/search' => 'recipes#search'
   post '/recipes/search' => 'recipes#search'
