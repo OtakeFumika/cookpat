c2214bbaf5c8bce3b1337c6f0888c1bfe3ac1962
pesu1234 <k.takafumi1110@gmail.com>
2017-06-27 11:15:14 +0900

    Fix column type

--- db/migrate/20170626025658_create_users.rb
+++ db/migrate/20170626025658_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :mail, null: false, unique: true
       t.string :image
       t.string :password, null: false, unique: true
-      t.integer :sex, null: false
+      t.string :sex, null: false
       t.integer :birthday, null: false
       t.integer :postnum, null: false
       t.timestamps
