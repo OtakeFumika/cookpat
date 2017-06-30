4e2cb5e21a91ed51fc00073660736392330a38ac
pesu1234 <k.takafumi1110@gmail.com>
2017-06-30 17:16:44 +0900

    Add mount_uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   enum sex: { 女性: '0', 男性: '1'}
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
