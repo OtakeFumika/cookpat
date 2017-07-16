de65323a6e796afd760ea53caaf977749b8da7d5
pesu1234 <k.takafumi1110@gmail.com>
2017-07-16 14:38:23 +0900

    Migrate reviews-table

--- db/migrate/20170716024213_create_reviews.rb
+++ db/migrate/20170716024213_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
-
+      t.text :review, null: false
+      t.float :rate
+      t.integer :recipe_id, null: false, foreign_key: true
+      t.integer :user_id, null: false, foreign_key: true
       t.timestamps
     end
   end
