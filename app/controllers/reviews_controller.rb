e743fb7d0c71371d4daa3fd9d18060086b700ff7
pesu1234 <k.takafumi1110@gmail.com>
2017-07-19 14:36:20 +0900

    Add create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,14 @@ class ReviewsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @review = Review.new
   end
+
+  def create
+    Review.create(create_params)
+    redirect_to controller: :recipes, action: :index
+  end
+
+  private
+  def create_params
+    params.require(:review).permit(:rate, :review, :title).merge(recipe_id: params[:recipe_id], user_id: current_user.id)
+  end
 end
