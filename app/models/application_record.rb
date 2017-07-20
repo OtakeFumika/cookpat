08481855e3c7a9fcc5edd62cb2c2ff05649d9385
pesu1234 <k.takafumi1110@gmail.com>
2017-07-20 20:22:43 +0900

    Set def review_average

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def review_average
+    average = self.reviews.average(:rate).to_f
+    (average.round(1) * 20).to_i
+  end
 end
